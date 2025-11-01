d5ee1515669423babc0346b55f658e0443353d10
Vedant Chainani <vedantchainani1084@gmail.com>
2025-11-01 18:20:58 +0530

    chore: add basic middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+// Middlewares
+import { compress } from "hono/compress";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
+import { requestId } from "hono/request-id";
+import { trimTrailingSlash } from "hono/trailing-slash";
 
 const app = new Hono();
+app.use(compress());
+app.use(cors());
+app.use(logger());
+app.use(prettyJSON());
+app.use(requestId());
+app.use(trimTrailingSlash());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
