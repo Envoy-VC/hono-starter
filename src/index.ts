2b98876f6f3b2eef4c2e4514a418004f8e094300
Vedant Chainani <vedantchainani1084@gmail.com>
2025-11-19 04:53:54 +0530

    chore: remove all

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,45 @@
-import { Scalar } from "@scalar/hono-api-reference";
+// import { Scalar } from "@scalar/hono-api-reference";
 import { Hono } from "hono";
+
 // Middlewares
 // import { compress } from "hono/compress";
-import { cors } from "hono/cors";
-import { logger } from "hono/logger";
-import { prettyJSON } from "hono/pretty-json";
-import { requestId } from "hono/request-id";
-import { trimTrailingSlash } from "hono/trailing-slash";
+// import { cors } from "hono/cors";
+// import { logger } from "hono/logger";
+// import { prettyJSON } from "hono/pretty-json";
+// import { requestId } from "hono/request-id";
+// import { trimTrailingSlash } from "hono/trailing-slash";
 // Plugins
-import { openAPIRouteHandler } from "hono-openapi";
+// import { openAPIRouteHandler } from "hono-openapi";
 
 // Routes
 import { routes } from "./routes";
 
 const app = new Hono();
 // app.use(compress());
-app.use(cors());
-app.use(logger());
-app.use(prettyJSON());
-app.use(requestId());
-app.use(trimTrailingSlash());
+// app.use(cors());
+// app.use(logger());
+// app.use(prettyJSON());
+// app.use(requestId());
+// app.use(trimTrailingSlash());
 
 // Routes
 app.route("/", routes);
 
 // OpenAPI
-app.get(
-  "/openapi.json",
-  openAPIRouteHandler(routes, {
-    documentation: {
-      info: {
-        description: "Simple Hono API",
-        title: "Hono Starter",
-        version: "1.0.0",
-      },
-    },
-  }),
-);
+// app.get(
+//   "/openapi.json",
+//   openAPIRouteHandler(routes, {
+//     documentation: {
+//       info: {
+//         description: "Simple Hono API",
+//         title: "Hono Starter",
+//         version: "1.0.0",
+//       },
+//     },
+//   }),
+// );
 
-app.get("/playground", Scalar({ url: "/openapi.json" }));
+// app.get("/playground", Scalar({ url: "/openapi.json" }));
 
 // biome-ignore lint/style/noDefaultExport: safe
 export default app;
